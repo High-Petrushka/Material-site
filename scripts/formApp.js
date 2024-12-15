d62c5433e5160f6f1ec49e28bb15d7ae416386c8
High-Petrushka <daniilprokopovic12@gmail.com>
2024-12-15 09:44:09 -0500

    Completing creation of the login form

--- scripts/formApp.js
+++ scripts/formApp.js
@@ -2,6 +2,29 @@ const logInForm = document.getElementById('login');
 
 const logInBtn = document.getElementById('login-btn');
 
+function listMaker(obj) {
+    let list = new Array;
+
+    for (i of obj) {
+        list.push(i);
+    }
+
+    return list;
+}
+
+const errors = listMaker(document.getElementsByClassName('error-box'));
+
+const nameError = errors.slice(0, 3);
+const birthError = errors.slice(3);
+
+const nameInp = logInForm.elements.name;
+const birthInp = logInForm.elements.birth;
+
+function showError(error) {
+    error.style.height = '25px';
+    error.style.margin = '10px 0 0 0';
+}
+
 logInBtn.addEventListener('click', (event) => {
     event.preventDefault();
 
@@ -18,11 +41,11 @@ logInBtn.addEventListener('click', (event) => {
         let nameValue = userName.value;
 
         if (nameValue == '') {
-            console.log('Is not filled!');
+            showError(nameError[0]);
         } else if (nameValue.length < 4) {
-            console.log('Too short!');
+            showError(nameError[1]);
         } else {
-            console.log('Unacceptable signs!');
+            showError(nameError[2]);
         }
     }
 
@@ -33,11 +56,11 @@ logInBtn.addEventListener('click', (event) => {
         let year = birthDay.value.slice(0, 4);
 
         if (year == '') {
-            console.log('Is not filled!');
+            showError(birthError[0]);
         } else if (Number(year) < 1950) {
-            console.log('Too old!');
+            showError(birthError[1]);
         } else {
-            console.log('Too yang!');
+            showError(birthError[2]);
         }
     }
 
@@ -47,5 +70,19 @@ logInBtn.addEventListener('click', (event) => {
         console.log(gender.value);
 
         logInForm.reset();
+    }
+});
+
+nameInp.addEventListener('input', () => {
+    for (i of nameError) {
+        i.style.height = '0';
+        i.style.margin = '0 0 0 0';
+    }
+});
+
+birthInp.addEventListener('input', () => {
+    for (i of birthError) {
+        i.style.height = '0';
+        i.style.margin = '0 0 0 0';
     }
 });
